df1a3e5da560fa541b82f85daf9ec7c59217acf3
Fabio Santos <fabiosantos.pe@gmail.com>
2023-02-23 07:53:03 -0300

    add 4clojure 51

---
+++ src/for_clojure/51.clj
@@ -0,0 +1,8 @@
+(ns for-clojure.51)
+
+"Here is an example of some more sophisticated destructuring.
+- https://4clojure.oxal.org/#/problem/51"
+
+(def C1 '(1 2 3 4 5))
+
+(println (= [1 2 [3 4 5] [1 2 3 4 5]] (let [[a b & c :as d] C1] [a b c d])))
